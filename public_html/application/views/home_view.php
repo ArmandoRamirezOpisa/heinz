28ec65921a10f9da173a205b5d181627705c7570
Armando Ramirez <armando.ramirez@opisa.com>
2019-01-14 13:40:15 -0600

    cambios menu

--- public_html/application/views/home_view.php
+++ public_html/application/views/home_view.php
@@ -15,52 +15,53 @@
         <script src="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/7.19.1/sweetalert2.min.js"></script>
     </head>
     <body class="bodyHome">
-        <div class="container-fluid">
-            <div class="row fixed-top mb-5">
-                <div class="col ">
-                    <nav class="navbar navbar-expand-lg navbar-dark NavColor">
-                        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
-                            <i class="fa fa-th text-white" aria-hidden="true"></i> <span class="ml-2 font-weight-bold">Puntos heinz</span>
-                        </button>
-                        <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
-                            <a class="navbar-brand  font-weight-bold" href="#"><img src="assets/images/kraft.png" width="140" > Saldo <span class="badge badge-warning"><?php echo number_format($this->session->userdata('puntos')); ?></span></a>
-                            <ul class="navbar-nav mr-auto mt-2 mt-lg-0 ml-lg-0">
-                                <li class="nav-item">
-                                    <a class="btn btn-light" href="javascript:void(0)" onclick="loadSection('cart_controller/showContentCart','dvSecc')"><i class="fa fa-shopping-basket mr-2" aria-hidden="true"></i>Carrito</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="btn btn-light " href="javascript:void(0)" onclick="loadSection('reglas_controller','dvSecc')"><i class="fa fa-list-alt mr-2" aria-hidden="true"></i>Reglas</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="btn btn-light " href="javascript:void(0)" onclick="loadSection('producto_controller','dvSecc')"><i class="fa fa-bullseye mr-2" aria-hidden="true"></i>Productos</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="btn btn-light " href="javascript:void(0)" onclick="loadSection('cart_controller/getCategory','dvSecc')"><i class="fa fa-gift mr-2" aria-hidden="true"></i>Premios</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="btn btn-light" href="javascript:void(0)" onclick="loadSection('canje_controller/getCanjes','dvSecc')"><i class="fa fa-shopping-cart mr-2" aria-hidden="true"></i>Movimientos</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="btn btn-light mr-lg-2" href="javascript:void(0)" onclick="loadSection('cuenta_controller','dvSecc')"><i class="fa fa-eye mr-2" aria-hidden="true"></i>Estado de cuenta</a>
-                                </li>
-                                <li class="nav-item ml-lg-1">
-                                    <div class="fb-like mt-1" data-href="https://www.facebook.com/Puntos-Heinz-380709378734414/?ref=br_rs" data-layout="button_count" data-action="like" data-size="large" data-show-faces="true" data-share="true"></div>
-                                </li>
-                            </ul>
-                            <form class="form-inline my-2 my-lg-0">
-                                <div class="dropdown">
-                                    <button class="btn btn-light dropdown-toggle" type="button" id="dropdownMenuButton2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                        <i class="fa fa-user-circle-o mr-1" aria-hidden="true"></i><?php echo $this->session->userdata('nombre'); ?>
-                                    </button>
-                                    <div class="dropdown-menu  " aria-labelledby="dropdownMenuButton2">
-                                        <a class="dropdown-item " id="exit" href="javascript:void(0)" onClick = "exit()"><i class="fa fa-sign-out mr-1" aria-hidden="true"></i>Salir</a>
-                                    </div>
-                                </div>
-                            </form>
+        <!--<div class="container-fluid">-->
+        <nav class="navbar navbar-expand-lg navbar-light color-navbar">
+			<a class="navbar-brand" href="http://puntosheinz.com.mx/">
+				<img src="assets/images/kraft.png" width="120" height="30" alt="">
+			</a>
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+			<div class="collapse navbar-collapse" id="navbarSupportedContent">
+				<ul class="navbar-nav mr-auto">
+					<li class="nav-item">
+						<a class="nav-link" href="#"><span class="colorMenu">Saldo</span><span class="badge badge-warning"><?php echo number_format($this->session->userdata('puntos')); ?></span></a>
+					</li>
+					<li class="nav-item btn-option">
+					<a class="nav-link" href="javascript:void(0)" onclick="loadSection('cart_controller/showContentCart','dvSecc')"><span class="colorMenu"><i class="fa fa-shopping-basket mr-2" aria-hidden="true"></i>Carrito</span></a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link btn-option" href="javascript:void(0)" onclick="loadSection('reglas_controller','dvSecc')"><span class="colorMenu"><i class="fa fa-list-alt mr-2" aria-hidden="true"></i>Reglas</span></a>
+					</li>
+					<li class="nav-item btn-option">
+					<a class="nav-link" href="javascript:void(0)" onclick="loadSection('producto_controller','dvSecc')"><span class="colorMenu"><i class="fa fa-bullseye mr-2" aria-hidden="true"></i>Productos</span></a>
+					</li>
+					<li class="nav-item btn-option">
+						<a class="nav-link" href="javascript:void(0)" onclick="loadSection('cart_controller/getCategory','dvSecc')"><span class="colorMenu"><i class="fa fa-gift mr-2" aria-hidden="true"></i>Premios</span></a>
+					</li>
+					<li class="nav-item btn-option">
+					<a class="nav-link" href="javascript:void(0)" onclick="loadSection('canje_controller/getCanjes','dvSecc')"><span class="colorMenu"><i class="fa fa-shopping-cart mr-2" aria-hidden="true"></i>Movimientos</span></a>
+					</li>
+					<li class="nav-item btn-option">
+						<a class="nav-link" href="javascript:void(0)" onclick="loadSection('cuenta_controller','dvSecc')"><span class="colorMenu"><i class="fa fa-eye mr-2" aria-hidden="true"></i>Estado de cuenta</span></a>
+					</li>
+					<li class="nav-item">
+						<div class="fb-like mt-1" data-href="https://www.facebook.com/Puntos-Heinz-380709378734414/?ref=br_rs" data-layout="button_count" data-action="like" data-size="large" data-show-faces="true" data-share="true"></div>
+					</li>
+				</ul>
+				<form class="form-inline my-2 my-lg-0">
+					<div class="dropdown">
+                        <button class="btn btn-danger dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            <i class="fas fa-user"></i> Usuario</button>
+                        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                            <a class="dropdown-item" href="javascript:void(0)" onClick = "exit()">Cerrar sesion</a>
                         </div>
-                    </nav>
-                </div>
-            </div>
+                    </div>
+
+				</form>
+			</div>
+		</nav>
             <div class="row justify-content-center mt-5">
                 <div class="col-12 col-md-4 mt-4">
                     <img src="assets/images/Logo_Login.png" class="img-fluid" alt="Responsive image">
