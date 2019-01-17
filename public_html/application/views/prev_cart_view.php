e128b4c102762049dd4ce2e4c9c919e5fced9ee9
Armando Ramirez <armando.ramirez@opisa.com>
2019-01-17 12:24:35 -0600

    cambios puntos heinz

--- public_html/application/views/prev_cart_view.php
+++ public_html/application/views/prev_cart_view.php
@@ -1,27 +1,27 @@
 <div class="col-md-10">
 	<h2>Contenido de la orden</h2>
 	<div class="table-responsive">
-	  <table class="table table-bordered">
-	    <thead>
-	    	<tr>
-	    		<th class="thHeadCart">C&oacute;digo</th>
-	    		<th class="thHeadCart">Nombre</th>
-	    		<th class="thHeadCart">Cantidad</th>
-	    		<th class="thHeadCart">Valor Puntos</th>
-                        <th class="thHeadCart"></th>
-	    	</tr>	
-	    </thead>
-	    <tbody id = "bodyContentCart">
-	    	
-	    </tbody>
-	  </table>
+		<table class="table table-bordered">
+	    	<thead>
+	    		<tr>
+	    			<th class="thHeadCart">C&oacute;digo</th>
+	    			<th class="thHeadCart">Nombre</th>
+	    			<th class="thHeadCart">Cantidad</th>
+	    			<th class="thHeadCart">Valor Puntos</th>
+                	<th class="thHeadCart"></th>
+	    		</tr>	
+	    	</thead>
+	    	<tbody id = "bodyContentCart">
+	    	</tbody>
+	  	</table>
 	</div>
     
     <br>
 
-    </div><br>
-    
-    <div style="text-align:left;float:left;"><h4>Direcci&oacute;n de env&iacute;o</h4>
+</div>
+<br>    
+<div style="text-align:left;float:left;">
+	<h4>Direcci&oacute;n de env&iacute;o</h4>
     <?php
         echo $this->session->userdata('calle')."<br>";
         echo $this->session->userdata('colonia')."<br>";
@@ -30,17 +30,13 @@
         echo $this->session->userdata('estado')."<br><br>";
     ?>
     
-    
     <a href="javascript:void(0)" onclick="loadSection('cart_controller/getCategory','dvSecc')" class="btn btn-primary" id="btnGenCanje">Continuar comprando<i class="fa fa-share ml-2" aria-hidden="true"></i></a>
 
+	<span id="btn">
+    	<a href="javascript:void(0)" onclick="sendCanje(<?php echo $this->session->userdata('puntos'); ?>,totPuntos)" class="btn btn-success" id="btnGenCanje">Finalizar compra <i class="fa fa-paper-plane ml-2" aria-hidden="true"></i></a>
+	</span>
 
-
-<span id="btn">
-    <a href="javascript:void(0)" onclick="sendCanje(<?php echo $this->session->userdata('puntos'); ?>,totPuntos)" class="btn btn-success" id="btnGenCanje">Finalizar compra <i class="fa fa-paper-plane ml-2" aria-hidden="true"></i></a>
-</span>
-
-
-    <center><label id="lblProc" style="display:none;"><h2 style="color:#084B8A;">Procesando ...</h2></label></center>
+	<center><label id="lblProc" style="display:none;"><h2 style="color:#084B8A;">Procesando ...</h2></label></center>
 </div>
 
    
@@ -49,9 +45,6 @@
 	var c = 1;
 	var ctd;
 	var totPuntos = 0;
-
-
-
 	
 	$.each(contOrder, function(k,v){
 		totPuntos = totPuntos + v.puntos;
@@ -70,10 +63,8 @@
 	str = "<tr><td colspan = 3 style ='text-align:right;'><h2>Total:</h2></td><td><h2>"+formatNumber.new(totPuntos)+"</h2></td></tr>";
 	$("#bodyContentCart").append(str);
 
-if (totPuntos == 0){
-var el = document.getElementById( 'btn' );
-el.parentNode.removeChild(el);
-
-}
-
+	if (totPuntos == 0){
+		var el = document.getElementById( 'btn' );
+		el.parentNode.removeChild(el);
+	}
 </script>
--- public_html/application/views/producto_view.php
+++ public_html/application/views/producto_view.php
@@ -1,164 +1,111 @@
 <div class="container">
-    <div class="row justify-content-center">
-          <div class="col-sm-12 col-md-9 ">
-                <div class="card-deck  mt-3 ">
-
-                    <div class="card ">
-                        <img class="card-img-top" src="assets/images/2018/1.jpg" alt="Card image cap">
-                        <div class="card-body">
-                            <p class=" card-title  font-weight-bold text-center">Sobre kétchup</p>
-                    </div>
+        <div class="row justify-content-center">
+                <div class="col-sm-12 col-md-9 ">
+                        <div class="card-deck  mt-3 ">
+                                <div class="card ">
+                                        <img class="card-img-top" src="assets/images/2018/1.jpg" alt="Card image cap">
+                                        <div class="card-body">
+                                                <p class=" card-title  font-weight-bold text-center">Sobre kétchup</p>
+                                        </div>
+                                </div>
+                                <div class="card">
+                                        <img class="card-img-top" src="assets/images/2018/2.jpg" alt="Card image cap">             
+                                        <div class="card-body">
+                                                <p class=" card-title  font-weight-bold text-center">BBQ Abal</p>
+                                        </div>
+                                </div>
+                                <div class="card">
+                                        <img class="card-img-top" src="assets/images/2018/3.jpg" alt="Card image cap">
+                                        <div class="card-body">
+                                                <p class=" card-title  font-weight-bold text-center">Aderezo de mayonesa abal</p>
+                                        </div>
+                                </div>
+                                <div class="card ">
+                                        <img class="card-img-top" src="assets/images/2018/4.jpg" alt="Card image cap">
+                                        <div class="card-body">
+                                                <p class=" card-title  font-weight-bold text-center">Bolsa de mayonesa de 3kg</p>
+                                        </div> 
+                                </div>
+                        </div>
+                        <div class="card-deck  mt-3">
+                                <div class="card">
+                                        <img class="card-img-top" src="assets/images/2018/5.jpg" alt="Card image cap">
+                                        <div class="card-body">
+                                                <p class=" card-title  font-weight-bold text-center"> Mostaza tarro</p>
+                                        </div>
+                                </div>
+                                <div class="card">
+                                        <img class="card-img-top" src="assets/images/2018/6.jpg" alt="Card image cap">
+                                        <div class="card-body">
+                                                <p class=" card-title  font-weight-bold text-center">    Ketchup red pet 397g </p>
+                                        </div>
+                                </div>
+                                <div class="card">
+                                        <img class="card-img-top" src="assets/images/2018/7.jpg" alt="Card image cap">
+                                        <div class="card-body">
+                                                <p class=" card-title  font-weight-bold text-center"> Blisters (miel, maple, mermelada)</p>
+                                        </div>
+                                </div>
+                                <div class="card">
+                                        <img class="card-img-top" src="assets/images/2018/8.jpg" alt="Card image cap">
+                                        <div class="card-body">
+                                                <p class=" card-title  font-weight-bold text-center"> Blisters (miel, maple, mermelada)</p>
+                                        </div>
+                                </div>
+                        </div>
+                        <div class="card-deck  mt-3 ">
+                                <div class="card">
+                                        <img class="card-img-top" src="assets/images/2018/9.jpg" alt="Card image cap">
+                                        <div class="card-body">
+                                                <p class=" card-title  font-weight-bold text-center"> Blisters (miel, maple, mermelada)</p>
+                                        </div>
+                                </div>
+                                <div class="card">
+                                        <img class="card-img-top" src="assets/images/2018/10.jpg" alt="Card image cap">
+                                        <div class="card-body">
+                                                <p class=" card-title  font-weight-bold text-center"> Mostaza 226.6 </p>
+                                        </div>
+                                </div>
+                                <div class="card">
+                                        <img class="card-img-top" src="assets/images/2018/11.jpg" alt="Card image cap">
+                                        <div class="card-body">
+                                                <p class=" card-title  font-weight-bold text-center">Parmesano 4.5lb</p>
+                                        </div>
+                                </div>
+                                <div class="card">
+                                        <img class="card-img-top" src="assets/images/2018/12.jpg" alt="Card image cap">
+                                        <div class="card-body">
+                                                <p class=" card-title  font-weight-bold text-center">Parmesano 8oz</p>
+                                        </div>
+                                </div>
+                        </div>
+                        <div class="card-deck  mt-3">
+                                <div class="card">
+                                        <img class="card-img-top" src="assets/images/2018/13.jpg" alt="Card image cap">
+                                        <div class="card-body">
+                                                <p class=" card-title  font-weight-bold text-center">Mayonesa 390g</p>
+                                        </div>
+                                </div>
+                                <div class="card">
+                                        <img class="card-img-top" src="assets/images/2018/14.jpg" alt="Card image cap">             
+                                        <div class="card-body">
+                                                <p class=" card-title  font-weight-bold text-center">Mayonesa ss</p>
+                                        </div>
+                                </div>
+                                <div class="card">
+                                        <img class="card-img-top" src="assets/images/2018/15.jpg" alt="Card image cap">
+                                        <div class="card-body">
+                                                <p class=" card-title  font-weight-bold text-center">Mostaza ss</p>
+                                        </div>
+                                </div>
+                                <div class="card">
+                                        <img class="card-img-top" src="assets/images/2018/16.jpg" alt="Card image cap">
+                                        <div class="card-body">
+                                                <p class=" card-title  font-weight-bold text-center">Mayonesa tarro</p>
+                                        </div>
+                                </div>
+                        </div>
+                </div>
+        </div>
 </div>
-
-
-                    <div class="card">
-                  <img class="card-img-top" src="assets/images/2018/2.jpg" alt="Card image cap">
-             
-      <div class="card-body">
-                            <p class=" card-title  font-weight-bold text-center">BBQ Abal</p>
-                    </div>
-                    </div>
-
-
-                <div class="card">
-                  <img class="card-img-top" src="assets/images/2018/3.jpg" alt="Card image cap">
-             
-  <div class="card-body">
-                            <p class=" card-title  font-weight-bold text-center">Aderezo de mayonesa abal</p>
-                    </div>
-                    </div>
-
-      <div class="card ">
-                        <img class="card-img-top" src="assets/images/2018/4.jpg" alt="Card image cap">
-                       <div class="card-body">
-                            <p class=" card-title  font-weight-bold text-center">Bolsa de mayonesa de 3kg</p>
-                    </div> 
-                    </div>
-
-          </div>
-
-
-
-                 <div class="card-deck  mt-3">
-
-              
-
-
-
-                    <div class="card">
-                  <img class="card-img-top" src="assets/images/2018/5.jpg" alt="Card image cap">
-            
-
-  <div class="card-body">
-                            <p class=" card-title  font-weight-bold text-center"> Mostaza tarro</p>
-                    </div>
-                    </div>
-
-
-                <div class="card">
-                  <img class="card-img-top" src="assets/images/2018/6.jpg" alt="Card image cap">
-         
- <div class="card-body">
-                            <p class=" card-title  font-weight-bold text-center">    Ketchup red pet 397g </p>
-                    </div>
-                    </div>
-
-                <div class="card">
-                  <img class="card-img-top" src="assets/images/2018/7.jpg" alt="Card image cap">
-             
-
-
-  <div class="card-body">
-                            <p class=" card-title  font-weight-bold text-center"> Blisters (miel, maple, mermelada)</p>
-                    </div>
-                    </div>
-
-                <div class="card">
-                  <img class="card-img-top" src="assets/images/2018/8.jpg" alt="Card image cap">
-               <div class="card-body">
-                            <p class=" card-title  font-weight-bold text-center"> Blisters (miel, maple, mermelada)</p>
-                    </div>
-                    </div>
-
-          </div>
-
-                  <div class="card-deck  mt-3 ">
-
-              
-
-
-
-                    <div class="card">
-                  <img class="card-img-top" src="assets/images/2018/9.jpg" alt="Card image cap">
-               <div class="card-body">
-                            <p class=" card-title  font-weight-bold text-center"> Blisters (miel, maple, mermelada)</p>
-                    </div>
-                    </div>
-
-
-                <div class="card">
-                  <img class="card-img-top" src="assets/images/2018/10.jpg" alt="Card image cap">
-            
- <div class="card-body">
-                            <p class=" card-title  font-weight-bold text-center"> Mostaza 226.6 </p>
-                    </div>
-                    </div>
-
-                <div class="card">
-                  <img class="card-img-top" src="assets/images/2018/11.jpg" alt="Card image cap">
-             <div class="card-body">
-                            <p class=" card-title  font-weight-bold text-center">Parmesano 4.5lb</p>
-                    </div>
-                    </div>
-
-                <div class="card">
-                  <img class="card-img-top" src="assets/images/2018/12.jpg" alt="Card image cap">
-             <div class="card-body">
-                            <p class=" card-title  font-weight-bold text-center">Parmesano 8oz</p>
-                    </div>
-                    </div>
-
-          </div>
-
-
-
-     <div class="card-deck  mt-3">
-              
-                    <div class="card">
-                  <img class="card-img-top" src="assets/images/2018/13.jpg" alt="Card image cap">
-               <div class="card-body">
-                            <p class=" card-title  font-weight-bold text-center">Mayonesa 390g</p>
-                    </div>
-                    </div>
-
-
-                <div class="card">
-                  <img class="card-img-top" src="assets/images/2018/14.jpg" alt="Card image cap">
-             
-  <div class="card-body">
-                            <p class=" card-title  font-weight-bold text-center">Mayonesa ss</p>
-                    </div>
-                    </div>
-
-                <div class="card">
-                  <img class="card-img-top" src="assets/images/2018/15.jpg" alt="Card image cap">
-             
-   <div class="card-body">
-                            <p class=" card-title  font-weight-bold text-center">Mostaza ss</p>
-                    </div>
-                    </div>
-
-                <div class="card">
-                  <img class="card-img-top" src="assets/images/2018/16.jpg" alt="Card image cap">
-              <div class="card-body">
-                            <p class=" card-title  font-weight-bold text-center">Mayonesa tarro</p>
-                    </div>
-                    </div>
-
-          </div>
-
-    </div>
-
-            </div>
-    </div>
 </div>
