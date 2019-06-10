64d31026da9c7df5b42e96bf6f37414e3ca37380
Armando Ramirez <armando.ramirez@opisa.com>
2019-06-10 14:21:56 -0500

    Se agregaron dos tablas

--- public_html/application/models/cuenta_model.php
+++ public_html/application/models/cuenta_model.php
@@ -5,7 +5,12 @@
         
             public function getEdoCuenta(){
                   $query = $this->db->query("
-                        CALL spu_hzGetEdoCuenta(".$this->session->userdata('programa').",".$this->session->userdata('participante').",".$this->session->userdata('empresa').");
+
+                        SELECT * FROM PartMovimientos
+                        WHERE codPrograma = ".$this->session->userdata('programa')."
+                        AND codParticipante = ".$this->session->userdata('participante')."
+                        AND codEmpresa = ".$this->session->userdata('empresa')."
+                        ORDER BY Act_Fecha DESC
                   ");
     		      if ($query->num_rows() > 0){
                         return $query->result_array(); 
