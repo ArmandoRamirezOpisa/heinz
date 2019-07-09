a4b20ef9575da3c0d1d463a43c1cb91d5122b3b3
Armando Ramirez <armando.ramirez@opisa.com>
2019-07-09 11:26:19 -0500

    cambios a la base de heinz

--- public_html/application/models/admin.php
+++ public_html/application/models/admin.php
@@ -22,7 +22,7 @@
                         ");
 
                         /* 
-                        INSERT INTO PreCanje
+                        INSERT INTO Canje
                         (codPrograma,idParticipante,noTipoEntrega)
                         VALUES (".$this->session->userdata('programa').",".$this->session->userdata('idPart').",1);
                         */
@@ -38,7 +38,7 @@
 			$nItem = 1;
 			foreach($datos as $d){
 	    		        $query = $this->db->query("
-                                        INSERT INTO PreCanjeDet (idParticipante,noFolio,idPreCanjeDet,CodPremio,
+                                        INSERT INTO CanjeDetalle (idParticipante,noFolio,idPreCanjeDet,CodPremio,
                                         cantidad,PuntosXUnidad)
 	    				VALUES (".$this->session->userdata('idPart').",".$noFolio.",".$nItem.",".$d->id.",".$d->cantidad.",".$d->puntos."/".$d->cantidad.")
 	                        ");
@@ -69,15 +69,17 @@
                         $query=$this->db->query("
                                 SELECT p.idCanje,p.feSolicitud,d.Cantidad,pr.Nombre_Esp,d.Status,d.Mensajeria,
                                 d.NumeroGuia,d.Cantidad*d.PuntosXUnidad *-1 as puntos
-                                FROM PreCanje p
-                                INNER JOIN PreCanjeDet d on d.noFolio = p.idCanje
+                                FROM Canje p
+                                INNER JOIN CanjeDetalle d on d.noFolio = p.idCanje
                                 INNER join Premio pr ON pr.codPremio = d.CodPremio 
                                 WHERE  
                                 p.codPrograma = ".$this->session->userdata('programa')."
                                 AND p.idParticipante = ".$this->session->userdata('idPart')."
                                 UNION ALL 
                                 SELECT p.NoFolio as idCanje, p.feMov as feSolicitud,null as Cantidad, 
-                                dsMov as Nombre_Esp, '-' as Status,'-' as Mensajeria,'-' as NumeroGuia,noPuntos as puntos from PartMovsRealizados p  where p.idParticipante = ".$this->session->userdata('idPart')."
+                                dsMov as Nombre_Esp, '-' as Status,'-' as Mensajeria,'-' as NumeroGuia,noPuntos as puntos 
+								from PartMovsRealizados p  
+								where p.idParticipante = ".$this->session->userdata('idPart')."
                                 ORDER BY 2,1
                         ");
                         if ($query->num_rows() > 0){
@@ -93,8 +95,8 @@
                                 concat(p.PrimerNombre,' ', p.SegundoNombre,' ' ,p.ApellidoPaterno,' ',p.ApellidoMaterno) as Nombre
                                 ,d.codPremio, d.Cantidad, pr.Nombre_Esp as Premio, 
                                 d.PuntosXUnidad*d.Cantidad as Puntos,d.PuntosXUnidad*d.Cantidad*0.0159 as Total
-                                FROM opisa_opisa.PreCanje c 
-                                JOIN opisa_opisa.PreCanjeDet d on c.idParticipante=d.idParticipante 
+                                FROM opisa_opisa.Canje c 
+                                JOIN opisa_opisa.CanjeDetalle d on c.idParticipante=d.idParticipante 
                                 and c.idCanje=d.noFolio
                                 JOIN opisa_opisa.Participante p on p.idParticipante=c.idParticipante
                                 JOIN opisa_opisa.Empresa e on e.codPrograma=p.codPrograma 
@@ -115,9 +117,10 @@
                                 SELECT pc.idCanje as Folio,p.codEmpresa,p.codParticipante,
                                 pc.feSolicitud as Fecha,pd.Status as Estatus,pd.Mensajeria,
                                 pd.NumeroGuia as Guias,pd.noFolio as FolioWeb,pd.codPremio,pd.Cantidad,
-                                pr.Nombre_Esp as Descripcion,pr.Marca,pd.PuntosXUnidad as Puntos,pd.PuntosXUnidad*pd.Cantidad as Total
-                                FROM PreCanje pc
-                                INNER JOIN PreCanjeDet pd ON pd.noFolio = pc.idCanje 
+                                pr.Nombre_Esp as Descripcion,pr.Marca,pd.PuntosXUnidad as Puntos,
+								pd.PuntosXUnidad*pd.Cantidad as Total
+                                FROM Canje pc
+                                INNER JOIN CanjeDetalle pd ON pd.noFolio = pc.idCanje 
                                 AND pd.idParticipante = pc.idParticipante
                                 INNER JOIN Participante p on p.idParticipante = pc.idParticipante
                                 INNER JOIN Premio pr on pr.codPremio = pd.codPremio
--- public_html/application/models/product_model.php
+++ public_html/application/models/product_model.php
@@ -5,7 +5,7 @@
         
             public function getProducts(){
     		      $query = $this->db->query("
-                        SELECT * FROM t12InProductoParticipante WHERE status = 1 ORDER BY id          
+                        SELECT * FROM ProductoParticipante WHERE status = 1 ORDER BY id          
                   ");
     		      if ($query->num_rows() > 0){
                         return $query->result_array(); 
